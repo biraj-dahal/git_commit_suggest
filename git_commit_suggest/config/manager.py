c004ba61ee8c71f3a8e81dff80d7d763f3b01483
biraj-dahal <dahalbiraj10@gmail.com>
2025-03-03 12:05:51 -0600

    files

--- git_commit_suggest/config/manager.py
+++ git_commit_suggest/config/manager.py
@@ -1,9 +1,13 @@
 import json
 import os
 from pathlib import Path
-from .schema import Config, ProviderConfig, Provider, Shell
+from schema import Config, ProviderConfig, Provider, Shell
 
 
 class ConfigManager:
     def __init__(self):
-        self.config_dir = Path
+        self.config_dir = Path.home() / "config" / "git-commit-suggest"
+        self.config_file = self.config_dir / "config.json"
+        print(self.config_dir)
+
+config_obj = ConfigManager()
